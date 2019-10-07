e75a7eedfabf993d5def9128c0b94bdce58c0e62
Diego Cardoso <diegocardoso.93@hotmail.com>
2019-10-06 21:39:45 -0300

    paddle select

--- breakout/Script/Highscores.gd
+++ breakout/Script/Highscores.gd
@@ -6,8 +6,13 @@ func _ready():
 	var file_read = File.new()
 	file_read.open('user://highscores.save', File.READ)
 	var highscores = file_read.get_var()
-	for score in highscores:
-		print(score)
+	if highscores:
+		for score in highscores:
+			print(score)
+
+func _process(delta):
+	if Input.is_action_pressed('ui_accept'):
+		get_tree().change_scene('res://Scene/Main.tscn')
 
 func save_score(new_score):
 	var file_save = File.new()
--- breakout/Script/Select.gd
+++ breakout/Script/Select.gd
@@ -1,8 +1,18 @@
 extends Node2D
 
+var selected_paddle = 0
+
 func _ready():
 	pass
 
 func _process(delta):
 	if Input.is_action_pressed('ui_accept'):
 		get_tree().change_scene('res://Scene/Start.tscn')
+
+func _on_PaddleLeft_pressed():
+	selected_paddle = selected_paddle - 1 if selected_paddle > 0 else 3
+	$PaddleType.set_cellv(Vector2(3, 2), selected_paddle)
+
+func _on_PaddleRight_pressed():
+	selected_paddle = selected_paddle + 1 if selected_paddle < 3 else 0
+	$PaddleType.set_cellv(Vector2(3, 2), selected_paddle)
